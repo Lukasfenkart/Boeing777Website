c7a4e931f829bf4e06bb4742a111e25b521ef275
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2023-06-16 12:43:02 +0200

    final finalupdate

--- src/Components/Variants-top.js
+++ src/Components/Variants-top.js
@@ -1,25 +1,25 @@
 function Vtop(){
             return(
                 <div className="flex flex-row p-5 pt-0">
-                    <div className="bg-slate-200 w-1/5 p-2 rounded-bl-xl rounded-br-md">
+                    <div className="bg-slate-100 w-1/5 p-2 rounded-bl-xl rounded-br-md">
                         777-200
                     </div>
-                    <div className="bg-slate-300 w-1/5 p-2 rounded-b-md">
+                    <div className="bg-slate-200 w-1/5 p-2 rounded-b-md">
                         777-200er 
                     </div>
-                    <div className="bg-slate-400 w-1/5 p-2 rounded-b-md">
+                    <div className="bg-slate-300 w-1/5 p-2 rounded-b-md">
                         777-200lr
                     </div>
-                    <div className="bg-slate-500 w-1/5 p-2 rounded-b-md">
+                    <div className="bg-slate-400 w-1/5 p-2 rounded-b-md">
                         777F
                     </div>
-                    <div className="bg-slate-600 w-1/5 p-2 rounded-b-md">
+                    <div className="bg-slate-500 w-1/5 p-2 rounded-b-md">
                         777-300
                     </div>
-                    <div className="bg-slate-700 w-1/5 p-2 rounded-br-xl rounded-bl-md">
+                    <div className="bg-slate-600 w-1/5 p-2 rounded-b-md">
                         777-300er 
                     </div>
-                    <div className="bg-slate-800 w-1/5 p-2 rounded-br-xl rounded-bl-md">
+                    <div className="bg-slate-700 w-1/5 p-2 rounded-br-xl rounded-bl-md">
                         777X-8 & 777X-9
                     </div>
                 </div>
